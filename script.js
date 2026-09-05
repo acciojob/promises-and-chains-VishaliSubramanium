cfbfde2552ac96402f0d822717c76b103b5fa32c
VishaliSubramanium <43952038+VishaliSubramanium@users.noreply.github.com>
2026-09-05 20:08:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const age = ageInput.value;
+  const name = nameInput.value.trim();
+
+  // Validate inputs
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Create Promise
+  const votingPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+
+    }, 4000);
+  });
+
+  // Handle Promise
+  votingPromise
+    .then(function (message) {
+      alert(message);
+    })
+    .catch(function (message) {
+      alert(message);
+    });
+});
